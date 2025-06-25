56a4886467699dec98e50053ff85d0e8c043cf14
gagadekar <135444819+gagadekar@users.noreply.github.com>
2025-06-25 13:10:53 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
        description = "The AWS region to create things in." 
-       default     = "us-east-1" 
+       default     = "ap-south-1" 
 }
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "Akshu" 
+    default     =  "Gani0909" 
 }
 
 variable "instance_type" { 
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-01816d07b1128cd2d" 
+    default     = "ami-0f918f7e67a3323f0" 
 }
 variable "versioning" {
     type        = bool
